83b062653ade600c0344002c14a08838085283e6
GE_CHENGMING <984603904@qq.com>
2016-09-18 13:39:35 +0800

    完善

---
+++ app/src/main/java/com/example/macavilang/adapter/HomeAdapter.java
@@ -0,0 +1,238 @@
+package com.example.macavilang.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.macavilang.jaguarfund_android.R;
+import com.example.macavilang.model.NetValueModel;
+import com.example.macavilang.model.TradeRecordModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by macavilang on 16/9/13.
+ */
+public class HomeAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private Context ctx;
+    private List allItemList;
+
+    public void setAllItemList(List allItemList) {
+        this.allItemList = allItemList;
+    }
+
+
+    public HomeAdapter(Context context, List groupItemList)
+    {
+        super();
+        this.mInflater = LayoutInflater.from(context);
+        this.allItemList = groupItemList;
+        this.ctx = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return allItemList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return allItemList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View view1 = view;
+        Object item = allItemList.get(i);
+        if(item instanceof String){
+            view1 = mInflater.inflate(R.layout.layout_customer_trade_record_list_item_tag,null);
+            TextView text = (TextView)view1.findViewById(R.id.groupName);
+            text.setText((CharSequence) getItem(i));
+        }else if(item instanceof TradeRecordModel){
+            TradeRecordModel tradeRecordModel = (TradeRecordModel)item;
+            view1 = mInflater.inflate(R.layout.layout_customer_trade_record_list_item,null);
+
+            TextView tradeInfo = (TextView)view1.findViewById(R.id.tradeInfo);
+            tradeInfo.setText(tradeRecordModel.getTradeRecordStr());
+
+            TextView perNetValueText = (TextView) view1.findViewById(R.id.perNetValue);
+            perNetValueText.setText("单位净值:" + tradeRecordModel.getUnitPriceView() + "元");
+
+            TextView amountMoneyText = (TextView) view1.findViewById(R.id.amountMoney);
+            amountMoneyText.setText("总金额:" + tradeRecordModel.getTradeAmount() + "元");
+
+            TextView tradeDateText = (TextView) view1.findViewById(R.id.tradeDate);
+            tradeDateText.setText(tradeRecordModel.getTradeDate());
+        }else if (item instanceof NetValueModel){
+            NetValueModel netValueModel = (NetValueModel)item;
+            view1 = mInflater.inflate(R.layout.layout_net_value_rencent_data_list_item,null);
+
+            TextView productNameText = (TextView) view1.findViewById(R.id.productName);
+            productNameText.setText(netValueModel.getProductName());
+
+            TextView perNetValueText = (TextView) view1.findViewById(R.id.prouctPerNetValue);
+            perNetValueText.setText("单位净值:" + netValueModel.getLatestMarketPrice() + "元");
+
+            TextView amountNetValueText = (TextView) view1.findViewById(R.id.productAmountNetValue);
+            amountNetValueText.setText("累计净值:" + netValueModel.getLatestAccumulativeMarketPrice() + "元");
+
+            TextView netValueDateText = (TextView) view1.findViewById(R.id.netValueDate);
+            netValueDateText.setText(netValueModel.getLatestMarketDate());
+        }
+
+
+        return view1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//方案二
+
+
+
+//    class GroupModel {
+//        private String title;
+//        private List<?> list;
+//
+//        public String getTitle() {
+//            return title;
+//        }
+//
+//        public void setTitle(String title) {
+//            this.title = title;
+//        }
+//
+//        public List<?> getList() {
+//            return list;
+//        }
+//
+//        public void setList(List<?> list) {
+//            this.list = list;
+//        }
+//
+//        public int getListSize(){
+//            return list.size()+1;
+//        }
+//
+//
+//        public Object getValue(int index){
+//            if(index==0){
+//                return title;
+//            }
+//            return list.get(index-1);
+//        }
+//
+//
+//
+//    }
+//
+//
+//    private class HomeAdapter extends BaseAdapter{
+//        private List<GroupModel> groups=new ArrayList<GroupModel>();
+//
+//        public void addGroup(GroupModel gl){
+//            groups.add(gl);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            int count=0;
+//            for(int i=0;i<groups.size();i++){
+//                count=count+groups.get(i).getListSize();
+//            }
+//            return count;
+//        }
+//
+//        @Override
+//        public Object getItem(int i) {
+//
+//            int count=0;
+//            Integer key=0;
+//            for(int j=0;j<groups.size();j++){
+//                key=j;
+//                Integer nowCount=count+groups.get(j).getListSize();
+//                if(i<nowCount){
+//                    break;
+//
+//                }
+//                count=nowCount;
+//            }
+//
+//            return groups.get(key).getValue(i-count);
+//        }
+//
+//        @Override
+//        public long getItemId(int i) {
+//            return i;
+//        }
+//
+//        @Override
+//        public View getView(int i, View view, ViewGroup viewGroup) {
+//            Object item=this.getItem(i);
+//            View view1 = view;
+//            if(item instanceof String){
+//                view1 = LayoutInflater.from(getContext()).inflate(R.layout.layout_customer_trade_record_list_item_tag,null);
+//                TextView text = (TextView)view1.findViewById(R.id.groupName);
+//                text.setText((CharSequence) getItem(i));
+//            }else if(item instanceof TradeRecordModel){
+//                TradeRecordModel tradeRecordModel = (TradeRecordModel)item;
+//                view1 = LayoutInflater.from(getContext()).inflate(R.layout.layout_customer_trade_record_list_item,null);
+//
+//                TextView tradeInfo = (TextView)view1.findViewById(R.id.tradeInfo);
+//                tradeInfo.setText(tradeRecordModel.getTradeRecordStr());
+//
+//                TextView perNetValueText = (TextView) view1.findViewById(R.id.perNetValue);
+//                perNetValueText.setText("单位净值:" + tradeRecordModel.getUnitPriceView() + "元");
+//
+//                TextView amountMoneyText = (TextView) view1.findViewById(R.id.amountMoney);
+//                amountMoneyText.setText("总金额:" + tradeRecordModel.getTradeAmount() + "元");
+//
+//                TextView tradeDateText = (TextView) view1.findViewById(R.id.tradeDate);
+//                tradeDateText.setText(tradeRecordModel.getTradeDate());
+//            }
+//
+////            if (groupTitle.contains(getItem(i))){
+////                view1 = LayoutInflater.from(getContext()).inflate(R.layout.layout_customer_trade_record_list_item_tag,null);
+////                TextView text = (TextView)view1.findViewById(R.id.groupName);
+////                text.setText((CharSequence) getItem(i));
+////            }else {
+////                TradeRecordModel tradeRecordModel = (TradeRecordModel)mainList.get(i);
+////                view1 = LayoutInflater.from(getContext()).inflate(R.layout.layout_customer_trade_record_list_item,null);
+////
+////                TextView tradeInfo = (TextView)view1.findViewById(R.id.tradeInfo);
+////                tradeInfo.setText(tradeRecordModel.getTradeRecordStr());
+////
+////                TextView perNetValueText = (TextView) view1.findViewById(R.id.perNetValue);
+////                perNetValueText.setText("单位净值:" + tradeRecordModel.getUnitPriceView() + "元");
+////
+////                TextView amountMoneyText = (TextView) view1.findViewById(R.id.amountMoney);
+////                amountMoneyText.setText("总金额:" + tradeRecordModel.getTradeAmount() + "元");
+////
+////                TextView tradeDateText = (TextView) view1.findViewById(R.id.tradeDate);
+////                tradeDateText.setText(tradeRecordModel.getTradeDate());
+////
+////            }
+//
+//            return view1;
+//        }
+//    }
---
+++ app/src/main/java/com/example/macavilang/model/NetValueModel.java
@@ -0,0 +1,80 @@
+package com.example.macavilang.model;
+
+/**
+ * Created by macavilang on 16/9/13.
+ */
+public class NetValueModel {
+    String productId;
+    String productName;
+    String latestMarketDate;
+    String latestMarketPrice;
+    String latestAccumulativeMarketPrice;
+    String previousMarketDate;
+    String previousMarketPrice;
+    String previousAccumulativeMarketPrice;
+
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLatestMarketDate() {
+        return latestMarketDate;
+    }
+
+    public void setLatestMarketDate(String latestMarketDate) {
+        this.latestMarketDate = latestMarketDate;
+    }
+
+    public String getLatestMarketPrice() {
+        return latestMarketPrice;
+    }
+
+    public void setLatestMarketPrice(String latestMarketPrice) {
+        this.latestMarketPrice = latestMarketPrice;
+    }
+
+    public String getLatestAccumulativeMarketPrice() {
+        return latestAccumulativeMarketPrice;
+    }
+
+    public void setLatestAccumulativeMarketPrice(String latestAccumulativeMarketPrice) {
+        this.latestAccumulativeMarketPrice = latestAccumulativeMarketPrice;
+    }
+
+    public String getPreviousMarketDate() {
+        return previousMarketDate;
+    }
+
+    public void setPreviousMarketDate(String previousMarketDate) {
+        this.previousMarketDate = previousMarketDate;
+    }
+
+    public String getPreviousMarketPrice() {
+        return previousMarketPrice;
+    }
+
+    public void setPreviousMarketPrice(String previousMarketPrice) {
+        this.previousMarketPrice = previousMarketPrice;
+    }
+
+    public String getPreviousAccumulativeMarketPrice() {
+        return previousAccumulativeMarketPrice;
+    }
+
+    public void setPreviousAccumulativeMarketPrice(String previousAccumulativeMarketPrice) {
+        this.previousAccumulativeMarketPrice = previousAccumulativeMarketPrice;
+    }
+}
