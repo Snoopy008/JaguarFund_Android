34e857eb2af3d0e766c0ca2205ee84bbe7be4930
GE_CHENGMING <984603904@qq.com>
2016-09-21 17:55:56 +0800

    添加界面

---
+++ app/src/main/java/com/example/macavilang/adapter/CustomerInverstProductListAdapter.java
@@ -0,0 +1,67 @@
+package com.example.macavilang.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.macavilang.jaguarfund_android.R;
+import com.example.macavilang.model.CustomerInverstProductModel;
+import com.example.macavilang.model.CustomerModel;
+import com.example.macavilang.model.ProductModel;
+
+import java.util.List;
+
+/**
+ * Created by macavilang on 16/9/21.
+ */
+public class CustomerInverstProductListAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private Context ctx;
+    private List<CustomerInverstProductModel> customerInverstProducts;
+
+    public CustomerInverstProductListAdapter(Context context, List<CustomerInverstProductModel> customerInverstProducts){
+        super();
+        this.mInflater = LayoutInflater.from(context);
+        this.ctx = context;
+        this.customerInverstProducts = customerInverstProducts;
+    }
+    @Override
+    public int getCount() {
+        return customerInverstProducts.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return customerInverstProducts.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View view1 = view;
+        CustomerInverstProductModel customerInverstProductModel = customerInverstProducts.get(i);
+        view1 = mInflater.inflate(R.layout.layout_customer_inverst_product_list_item,null);
+
+        TextView customerDetail_customerInverst_inverstAmount = (TextView)view1.findViewById(R.id.customerDetail_customerInverst_inverstAmount);
+        customerDetail_customerInverst_inverstAmount.setText(customerInverstProductModel.getInvestAmount());
+
+        TextView customerDetail_customerInverst_productName = (TextView) view1.findViewById(R.id.customerDetail_customerInverst_productName);
+        customerDetail_customerInverst_productName.setText(customerInverstProductModel.getFundName());
+
+        TextView customerDetail_customerInverst_inverstShare = (TextView) view1.findViewById(R.id.customerDetail_customerInverst_inverstShare);
+        customerDetail_customerInverst_inverstShare.setText(customerInverstProductModel.getNetValue());
+
+        TextView customerDetail_customerInverst_totalNetValue = (TextView) view1.findViewById(R.id.customerDetail_customerInverst_totalNetValue);
+        customerDetail_customerInverst_totalNetValue.setText(customerInverstProductModel.getTotalNetValue());
+
+        return view1;
+    }
+}
---
+++ app/src/main/java/com/example/macavilang/model/CustomerInverstProductModel.java
@@ -0,0 +1,61 @@
+package com.example.macavilang.model;
+
+/**
+ * Created by macavilang on 16/9/21.
+ */
+public class CustomerInverstProductModel {
+    String productId;
+    String fundName;
+    String investShare;
+    String investAmount;
+    String netValue;
+    String totalNetValue;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getFundName() {
+        return fundName;
+    }
+
+    public void setFundName(String fundName) {
+        this.fundName = fundName;
+    }
+
+    public String getInvestShare() {
+        return investShare;
+    }
+
+    public void setInvestShare(String investShare) {
+        this.investShare = investShare;
+    }
+
+    public String getInvestAmount() {
+        return investAmount;
+    }
+
+    public void setInvestAmount(String investAmount) {
+        this.investAmount = investAmount;
+    }
+
+    public String getNetValue() {
+        return netValue;
+    }
+
+    public void setNetValue(String netValue) {
+        this.netValue = netValue;
+    }
+
+    public String getTotalNetValue() {
+        return totalNetValue;
+    }
+
+    public void setTotalNetValue(String totalNetValue) {
+        this.totalNetValue = totalNetValue;
+    }
+}
